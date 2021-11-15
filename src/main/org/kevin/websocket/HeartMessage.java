214a171ba96f525e1cca0e80c0f5f2b2d357cded
Kevin <lexdxw@126.com>
2021-11-15 22:56:35 +0800

    Add maven for websocket

---
+++ src/main/org/kevin/websocket/HeartMessage.java
@@ -0,0 +1,13 @@
+package org.kevin.websocket;
+
+public class HeartMessage {
+    String clientId;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+}
